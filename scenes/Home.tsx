f8bacaf94b5788ecd065cd15d8a83989d52b7937
jaydainn <37037667+jaydainn@users.noreply.github.com>
2020-06-01 00:40:32 +0200

    update fake server

---
+++ scenes/Home.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react"
+import {Text, View, Dimensions , StyleSheet} from "react-native"
+import {Header, Left, Right ,Body, Footer, Spinner} from "native-base"
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps"
+import moment from "moment"
+import "moment/locale/fr"
+const styles = StyleSheet.create({
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height ,
+        zIndex: 0
+      },
+      bottomBar : {
+          width : Dimensions.get('window').width ,
+          height : Dimensions.get('window').height/100 * 20 ,
+          backgroundColor : "#222222" , 
+          bottom :0 ,
+          borderTopLeftRadius : 23,
+          borderTopRightRadius : 23,
+          zIndex:2,
+          position: "absolute",
+          padding:"5%"
+      }
+
+      
+})
+
+const Home = () => {
+    const [time , setTime] = useState<any>()
+    const [latitude , setLatitude] = useState<any>()
+    const [longitude , setLongitude] = useState<any>()
+
+    useEffect(() => {
+        moment().locale('fr')
+        fetch("http://192.168.1.32:420/api")
+            .then((response) => {return response.json()})
+            .then((obj) => {setTime(obj.time) ; setLatitude(obj.coordinates.latitude) ; setLongitude(obj.coordinates.longitude)})
+        const interval = setInterval(() => {
+            fetch("http://192.168.1.32:420/api")
+            .then((response) => {return response.json()})
+            .then((obj) => {setTime(obj.time) ; setLatitude(obj.coordinates.latitude) ; setLongitude(obj.coordinates.longitude)})
+        },60000)   
+    } , [])
+
+    
+    
+
+   
+
+    
+    return (
+        
+        <View  >
+            <MapView  style={styles.mapStyle} >
+                {longitude? 
+                <Marker title="🐢" description={moment(time).format("LTS")} coordinate={{latitude : latitude , longitude : longitude}} />
+                :
+                <></>
+                
+            
+            }
+
+                
+            
+            </MapView>
+            <View  style={styles.bottomBar}>
+                {time?
+                <View>
+                    <Text  style={{color: "white" , fontWeight : "800" ,fontSize:34}}>{moment(time).format('LTS')}</Text>
+                    <Text style={{color: "white" ,fontWeight : "500", fontSize:24}}>Derniere position: </Text>
+                    <View style={{paddingBottom :10 , paddingTop : 10}}>
+                    <Text style={{color: "white"}}>{latitude}</Text>
+                    <Text style={{color: "white"}}>{longitude}</Text>
+                    </View>
+                </View>
+                 :
+                <Spinner color="white"></Spinner>
+                
+            }
+
+                
+                
+            </View>
+            
+          
+        </View>
+      
+    )
+}
+
+
+
+
+
+export default Home
